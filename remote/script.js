ff72876ab9207e85bd37cbc88701e498b5a9c993
thomas <cornu.thomas6@gmail.com>
2023-11-03 14:12:05 +0100

    remote

---
+++ remote/script.js
@@ -0,0 +1,70 @@
+const modeSwitch = document.getElementById('modeSwitch');
+const switchText = document.getElementById('switchText');
+const colorPicker2 = document.getElementById('colorPicker2');
+
+modeSwitch.addEventListener('change', function () {
+    if (modeSwitch.checked) {
+        switchText.textContent = 'Dégradé';
+        colorPicker2.classList.remove('hidden');
+    } else {
+        switchText.textContent = 'Couleur Simple';
+        colorPicker2.classList.add('hidden');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const colorPicker1 = document.getElementById('colorPicker1');
+    const colorPicker2 = document.getElementById('colorPicker2');
+    const modeSwitch = document.getElementById('modeSwitch');
+
+    // Initialisez Socket.io et connectez-vous au serveur
+    const socket = io.connect('http://localhost:3000'); // Remplacez par l'URL de votre serveur Socket.io
+
+    // Écoutez les changements de couleur dans le colorPicker1
+    colorPicker1.addEventListener('change', function() {
+        const color1 = colorPicker1.value;
+        const color2 = colorPicker2.value;
+        const backgroundType = modeSwitch.checked ? 'gradient' : 'one';
+
+        const data = {
+            background_type: backgroundType,
+            background_color_1: color1,
+            background_color_2: color2,
+        };
+
+        // Émettez les données au format JSON via Socket.io
+        socket.emit('colorChange', JSON.stringify(data));
+    });
+
+    // Écoutez les changements de couleur dans le colorPicker2
+    colorPicker2.addEventListener('change', function() {
+        const color1 = colorPicker1.value;
+        const color2 = colorPicker2.value;
+        const backgroundType = modeSwitch.checked ? 'gradient' : 'one';
+
+        const data = {
+            background_type: backgroundType,
+            background_color_1: color1,
+            background_color_2: color2,
+        };
+
+        // Émettez les données au format JSON via Socket.io
+        socket.emit('colorChange', JSON.stringify(data));
+    });
+
+    // Écoutez les changements d'état de la checkbox
+    modeSwitch.addEventListener('change', function() {
+        const color1 = colorPicker1.value;
+        const color2 = colorPicker2.value;
+        const backgroundType = modeSwitch.checked ? 'gradient' : 'one';
+
+        const data = {
+            background_type: backgroundType,
+            background_color_1: color1,
+            background_color_2: color2,
+        };
+
+        // Émettez les données au format JSON via Socket.io
+        socket.emit('colorChange', JSON.stringify(data));
+    });
+});
