5b2d1dfe5851b1cb73dc47c0aae82fab2af9c9df
Antoine <antoine.raimand@gmail.com>
2023-11-03 11:46:44 +0100

    Refactored background color handling and added JSON file fetching

--- background/script.js
+++ background/script.js
@@ -1,33 +1,47 @@
-function traitement(json){
-    console.log(json.background_type)
-    if (json.background_type == "one"){
-        console.log("AAAAAAAA")
-        let bgColor = json.background_color_1
-        console.log(bgColor)
-        let body = document.getElementById('main-container')
-        body.style.backgroundColor = bgColor
-    } else if (json.background_type == "gradiant"){
-        let bgColor1 = json.background_color_1
-        let bgColor2 = json.background_color_2
-        let body = document.getElementById('main-container')
-        body.style.backgroundColor = linear-gradient(bgColor1, bgColor2);
-    } else {
-        console.log("Error type not found")
+//Handling the JSON file and changing the background color
+function handleColor(json) {
+
+    let background_type = json.background_type
+    let div = document.getElementById('main-container')
+
+    switch (background_type) {
+        case "one":
+            let background_color = json.background_color_1
+
+            div.style.backgroundColor = background_color
+            div.style.height = "100vh"
+            div.style.width = "100vw"
+
+            break;
+        case "gradient":
+            let background_color_1 = json.background_color_1
+            let background_color_2 = json.background_color_2
+            let gradient = `linear-gradient(to right, ${background_color_1}, ${background_color_2})`
+
+            div.style.background = gradient;
+            div.style.height = "100vh"
+            div.style.width = "100vw"
+
+            break;
+        default:
+            console.log("Error type not found")
+
+            break;
     }
 }
 
-
+//Fetching the JSON file
 fetch('./OneColor.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erreur de réseau');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    traitement(data)
-  })
-  .catch(error => {
-    console.error('Erreur : ' + error);
-  });
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erreur de réseau');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        handleColor(data)
+    })
+    .catch(error => {
+        console.error('Erreur : ' + error);
+    });
